3a8517314426e3c47d64beb965462c462bb8fd2a
abc111-debug <75261687+abc111-debug@users.noreply.github.com>
2020-12-05 19:01:34 +0800

    Add files via upload

--- tools/smalibat.py
+++ tools/smalibat.py
@@ -8,16 +8,15 @@ def decompilation(filename,outdir):
     command = "apktool.jar d {0} -o {1}".format(filename, outdir)
     os.system(command)
 
-#反编译
 if __name__ == '__main__':
     index=0
-    # apk location
+    #apk
     path = "VirusShare2018\\Virsus2019\\"
     apklist = os.listdir(path)
     time_start = time.time()
 
     for apk in apklist:
-        outdir="out-of-sample-2019\\"+apk  #反编译的输出地址
+        outdir="out-of-sample-2019\\"+apk
         filename=path+apk
         print(filename)
         print(index)
